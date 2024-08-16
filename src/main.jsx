8125b976f8bb27768cdf0107881517f7eb2eb66d
David Fernadez <daffevafernandez@gmail.com>
2024-08-16 17:24:27 +0200

    feat: creation of handleChange and login function

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Envuelve tu aplicación en BrowserRouter */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </StrictMode>,
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,9 +1,42 @@
+import React, { useState } from 'react'
 import "./Login.css";
 
-const Login = () => {
-  return (
-    <h1>LOGIN</h1>
-  );
-};
+export const Login = () => {
+    const [credentials, setCredentials] = useState(
+        {
+          email: "",
+          password: ""
+        }
+      )
 
+      function handleChange(e) {
+        console.log('handleChange');
+        setCredentials(prevState => (
+          {
+            ...prevState,
+            [e.target.name]: e.target.value
+          }
+        ))
+      }
+
+      function login() {
+        console.log('Login')
+        console.log(credentials)
+      }
+
+      return (
+        <>
+          <h1>Login</h1>
+          <div>
+            <input type="email" name="email" placeholder='Email' onChange={handleChange} />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder='Password' onChange={handleChange} />
+          </div>
+          <div>
+            <input type="button" value="Login" onClick={login} />
+          </div>
+        </>
+      )
+}
 export default Login;
