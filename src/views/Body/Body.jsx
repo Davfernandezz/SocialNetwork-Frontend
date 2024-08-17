085bd49735c39457f88ca59dc7fa78e494b08f71
David Fernadez <daffevafernandez@gmail.com>
2024-08-17 16:39:08 +0200

    feat: creation of view and admin route

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -4,6 +4,7 @@ import Login from "../Login/Login";
 import Register from '../Register/Register';
 import { Home } from '../Home/Home';
 import { Profile } from '../Profile/Profile';
+import { Admin } from '../Admin/Admin';
 
 function Body() {
   return (
@@ -13,6 +14,8 @@ function Body() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* {role === 2 && <Route path="/admin" element={<Admin />} />} */}
       </Routes>
     </>
   );
