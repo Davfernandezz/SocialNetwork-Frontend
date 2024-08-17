b4206879d897aa3156c5230a565495b81651e796
David Fernadez <daffevafernandez@gmail.com>
2024-08-17 16:30:30 +0200

    feat: creation of view and home route

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from "../Login/Login";
 import Register from '../Register/Register';
+import { Home } from '../Home/Home';
 
 function Body() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
