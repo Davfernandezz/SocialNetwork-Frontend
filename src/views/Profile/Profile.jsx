4c419bc4efbfc8d26e35c4d625fa2ed475bd068b
David Fernadez <daffevafernandez@gmail.com>
2024-08-18 16:34:28 +0200

    feat: profile view structure

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Profile.css";
+import { CInput } from '../../components/CInput/CInput';
+import { useNavigate } from 'react-router-dom';
 
 export const Profile = () => {
+  const [profileData, setProfileData] = useState({ name: "", email: "", is_active: "" })
+
+  const passport = JSON.parse(localStorage.getItem("passport"))
+  let token;
+  const navigate = useNavigate()
+
+  useEffect(() => {
+      if (!passport) {
+          navigate("/login")
+      } else {
+          token = passport.token
+      }
+  }, [])
+
+  const logout = () => {
+      localStorage.removeItem("passport")
+      console.log("Closed session")
+  }
+
   return (
-    <div><h1>Profile</h1></div>
+      <>
+          <h1>Profile</h1>
+          <p>Name: {profileData.name}</p>
+          <p>Email: {profileData.email}</p>
+          <p>Created_at: {profileData.is_active}</p>
+          <CInput type="button" name="logout" value="logout" emitFunction={logout} />
+      </>
   )
 }
