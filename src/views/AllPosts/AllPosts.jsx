1ad8c86c6769aff6d42cfbc320b2e09ef3d945b1
David Fernadez <daffevafernandez@gmail.com>
2024-08-20 18:07:48 +0200

    feat: allPosts functions and structure

--- src/views/AllPosts/AllPosts.jsx
+++ src/views/AllPosts/AllPosts.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 
 export const AllPosts = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    const navigate = useNavigate();
+  
+    const { passport } = useAuth();
+  
+    useEffect(() => {
+      const bringAllPosts = async () => {
+        try {
+          const response = await getAllPosts();
+          setPosts(response.data);
+        } catch (error) {
+          console.error("Error fetching services:", error);
+        }
+      };
+  
+      bringAllPosts();
+    }, []);
+
   return (
-    <div>AllPosts</div>
-  )
-}
+    <div>
+      <h1>Posts</h1>
+      {services.map((posts) => (
+        <div key={posts.id}>
+          <CCard description={posts.description} />
+        </div>
+      ))}
+    </div>
+);
+};
