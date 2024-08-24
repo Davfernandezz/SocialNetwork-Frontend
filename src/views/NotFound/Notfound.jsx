8cfe7b8737dcf688ed9c2717d96e15e918e6c7b5
David Fernadez <daffevafernandez@gmail.com>
2024-08-24 17:44:13 +0200

    feat: notFound view layout

--- src/views/NotFound/Notfound.jsx
+++ src/views/NotFound/Notfound.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
 import "./NotFound.css";
 
 export const NotFound = () => {
   return (
-    <div><h1>NotFound</h1></div>
-  )
-}
+    <div className="notfound-wrapper">
+      <div className="notfound-container">
+        <h1 className="notfound-title">404</h1>
+        <p className="notfound-message">Page Not Found</p>
+        <p className="notfound-description">
+        Sorry, the page you are looking for does not exist. 
+        </p>
+        <a href="/" className="notfound-link">Back to Home</a>
+      </div>
+    </div>
+  );
+};
